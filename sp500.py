8b8a207d786fc96deb2f99b35972b2eeb5bd144d
tomherbert-jazz <tomherbertjazz@gmail.com>
2024-01-07 23:08:22 -0500

    commit 1/7/2024 Tom xxx

--- sp500.py
+++ sp500.py
@@ -18,7 +18,7 @@ st.sidebar.header('User Input Features')
 
 # Web scraping of S&P 500 data
 # xxx
-@st.cache
+@st.cache_data
 def load_data():
     url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     html = pd.read_html(url, header = 0)
@@ -63,16 +63,28 @@ data = yf.download(
     )
 
 # Plot Closing Price of Query Symbol
+#def price_plot(symbol):
+ # df = pd.DataFrame(data[symbol].Close)
+  #plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
+  #plt.xticks(rotation=90)
+  #plt.title(symbol, fontweight='bold')
+  #plt.xlabel('Date', fontweight='bold')
+  #plt.ylabel('Closing Price', fontweight='bold')
+  #return st.pyplot()
+  
 def price_plot(symbol):
   df = pd.DataFrame(data[symbol].Close)
   df['Date'] = df.index
-  plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
-  plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
+
+  fig, ax = plt.subplots()
+  ax.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
+  ax.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
   plt.xticks(rotation=90)
   plt.title(symbol, fontweight='bold')
   plt.xlabel('Date', fontweight='bold')
   plt.ylabel('Closing Price', fontweight='bold')
-  return st.pyplot()
+
+  return st.pyplot(fig)
 
 num_company = st.sidebar.slider('Number of Companies', 1, 5)
 
