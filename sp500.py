bda6bdb33ba27db960600a59179954628dd5b3b0
tomherbert-jazz <tomherbertjazz@gmail.com>
2024-01-07 22:36:09 -0500

    commit 1/7/2024 Tom xxx

--- sp500.py
+++ sp500.py
@@ -18,7 +18,7 @@ st.sidebar.header('User Input Features')
 
 # Web scraping of S&P 500 data
 # xxx
-@st.cache_resource
+@st.cache
 def load_data():
     url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     html = pd.read_html(url, header = 0)
